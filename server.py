bc1665ea9de13bb66c01c95e91c6fd8f34b7d870
icos w <wicos@wicos.cn>
2021-11-09 00:32:02 +0800

    添加

--- server.py
+++ server.py
@@ -1,68 +1,67 @@
 # -*- encoding: utf-8 -*-
-"""
-@File    :   chat.py
-@Time    :   2021/10/23 21:12:03
+'''
+@File    :   server.py
+@Time    :   2021/11/08 22:08:30
 @Author  :   Wicos 
 @Version :   1.0
 @Contact :   wicos@wicos.cn
 @Blog    :   https://www.wicos.me
-"""
-
+'''
 
 # here put the import lib
+from websocket_server import WebsocketServer
 from threading import Thread
-from queue import Queue
-import asyncio
-from websockets import serve
 import json
+import time
+import logging
 
 
-class CHAT():
+class SERVER():
     def __init__(self):
-        super(CHAT, self).__init__()
-        self.daemon = True
-        self.USER = set()
+        super(SERVER,self).__init__()
+        self.USERS = []
         self.MESSAGE = []
+        self.server = WebsocketServer(host="0.0.0.0",port=4456)
+        self.server.set_fn_new_client(self.client_connect)
+        self.server.set_fn_client_left(self.client_left)
+        self.server.set_fn_message_received(self.message_received)
 
-    def login(self,uid):
-        self.USER.add(uid)
-
-    def logout(self, ws,uid):
-        if uid in self.USER:
-            self.USER.remove(uid)
-        else:
-            ws.close()
+    def therad_fun(self):
+        for msg in self.MESSAGE:
+            for user in self.USERS:
+                if msg["to"] == user["uid"]:
+                    self.server.send_message(user["client"],msg["data"])
+                    self.MESSAGE.remove(msg)
+    
+    def thread_main(self):
+        while True:
+            self.therad_fun()
+            time.sleep(0.5)
+    
+    def client_connect(self,client,server):
+        self.USERS.append({"client":client,"id":client["id"]})
+    
+    # 当旧的客户端离开
+    def client_left(self,client, server):
+        print("客户端%s断开" % client['id'])
+    
+    # 接收客户端的信息。
+    def message_received(self,client, server, message):
+        data = json.loads(message)
+        if data["status"] == "login":
+            for user in self.USERS:
+                if client["id"] == user["id"]:
+                    user["uid"] = data["uid"]
+        elif data["status"] == "logout":
+            for user in self.USERS:
+                if client["id"] == user["id"]:
+                    self.USERS.remove(user)
+        elif data["status"] == "chat":
+            self.MESSAGE.append(data)
             
-    def rec_msg(self,data):
-        pass
-
-    def recv_msg(self, ws, recv_data):
-        recvmsg = json.loads(recv_data)
-        uid = recvmsg["uid"]
-        status = recvmsg["status"]
-        if status == "login":
-            self.login(uid)
-        elif status == "logout":
-            self.logout(ws,uid)
-        else:
-            if recvmsg["to"] in self.USER_DICT.values():
-                pass
-
-    def send_msg(self, ws, send_data):
-        pass
-
-    async def main_fun(self, websocket, path):
-        async for message in websocket:
-            await websocket.send(message)
-
-    async def main_loop(self):
-        async with serve(self.main_fun, "localhost", 4456):
-            await asyncio.Future()  # run forever
-
     def run(self):
-        print("start chat server")
-        asyncio.run(self.main_loop())
-
-if __name__ == "__main__":
-    chat = CHAT()
-    chat.run()
+        Thread(target=self.thread_main).start()
+        self.server.run_forever()
+        
+a = SERVER()
+a.run()
